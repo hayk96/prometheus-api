f9800bcc3ce18ff9ccfa3471943761b65688c42b
hayk96 <hayko5999@gmail.com>
2024-04-13 20:47:31 +0400

    Fix selection  of the alerting and recording rules
    Signed-off-by: hayk96 <hayko5999@gmail.com>

--- frontend/script.js
+++ frontend/script.js
@@ -1,28 +1,113 @@
+let filesData = [];
 let currentFilename = '';
 let codeMirrorInstance;
-let filesData = [];
 
-function preprocessDataForYaml(selectedGroup) {
-    let cleanGroupData = JSON.parse(JSON.stringify(selectedGroup));
-
-    delete cleanGroupData.file;
-    delete cleanGroupData.interval;
-    delete cleanGroupData.limit;
-    delete cleanGroupData.evaluationTime;
-    delete cleanGroupData.lastEvaluation;
-
-    cleanGroupData.rules = cleanGroupData.rules.map(rule => {
-        return {
-            alert: rule.name,
-            expr: rule.query,
-            for: convertDurationToHumanReadable(rule.duration),
-            labels: rule.labels,
-            annotations: rule.annotations
-        };
+document.addEventListener('DOMContentLoaded', () => {
+    codeMirrorInstance = CodeMirror.fromTextArea(document.getElementById('yamlEditor'), {
+        mode: 'yaml',
+        lineNumbers: true,
+        theme: 'monokai',
+        lineWrapping: true
     });
 
-        if (selectedGroup.type === 'alerting') {
-        cleanGroupData.rules = cleanGroupData.rules.map(rule => {
+    document.getElementById('editAlertingRulesBtn').addEventListener('click', () => {
+        fetchAndDisplayRules('alert');
+    });
+
+    document.getElementById('editRecordingRulesBtn').addEventListener('click', () => {
+        fetchAndDisplayRules('record');
+    });
+
+    document.getElementById('fileSelector').addEventListener('change', (event) => {
+        const selectedIndex = event.target.value;
+        currentFilename = filesData[selectedIndex].file;
+        const selectedGroup = filesData[selectedIndex];
+        const preprocessedData = preprocessDataForYaml(selectedGroup);
+        codeMirrorInstance.setValue(jsyaml.dump(preprocessedData));
+        document.getElementById('editorContainer').style.display = 'block';
+    });
+
+    document.getElementById('cancelBtn').addEventListener('click', () => {
+        codeMirrorInstance.setValue('');
+        document.getElementById('editorContainer').style.display = 'none';
+    });
+
+    document.getElementById('saveBtn').addEventListener('click', () => {
+        const editedYaml = codeMirrorInstance.getValue();
+        try {
+            const modifiedData = jsyaml.load(editedYaml);
+            const payload = JSON.stringify({ data: modifiedData });
+            const filename = encodeURIComponent(currentFilename.split('/').pop());
+
+            fetch(`http://localhost:5000/api/v1/rules/${filename}?recreate=true`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: payload
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                alert('Rule saved successfully.');
+                document.getElementById('editorContainer').style.display = 'none';
+            })
+            .catch(error => {
+                alert('Error saving rule: ' + error.message);
+                console.error('Error:', error);
+            });
+        } catch (error) {
+            alert('Error processing YAML: ' + error.message);
+            console.error('Error parsing YAML:', error);
+        }
+    });
+});
+
+function fetchAndDisplayRules(ruleType) {
+    fetch(`http://localhost:5000/api/v1/rules?type=${ruleType}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(json => {
+            if (!json.data) {
+                throw new Error('No data found in the response');
+            }
+            filesData = json.data.groups;
+            populateFileSelector(filesData);
+        })
+        .catch(error => {
+            console.error(`Error fetching ${ruleType} rules:`, error);
+        });
+}
+
+function populateFileSelector(groups) {
+    const fileSelector = document.getElementById('fileSelector');
+    fileSelector.innerHTML = '';
+    groups.forEach((group, index) => {
+        const option = new Option(group.file, index);
+        fileSelector.add(option);
+    });
+    fileSelector.style.display = 'block';
+    fileSelector.selectedIndex = -1;
+}
+
+function preprocessDataForYaml(selectedGroup) {
+    let groupType = selectedGroup.rules[0]?.type;
+
+    if (!groupType) {
+        console.error('Rule type is undefined.');
+        return null;
+    }
+
+    let processedRules = selectedGroup.rules.map(rule => {
+        if (rule.type === 'alerting') {
             return {
                 alert: rule.name,
                 expr: rule.query,
@@ -30,19 +115,22 @@ function preprocessDataForYaml(selectedGroup) {
                 labels: rule.labels,
                 annotations: rule.annotations
             };
-        });
-    } else if (selectedGroup.type === 'recording') {
-        cleanGroupData.rules = cleanGroupData.rules.map(rule => {
+        } else if (rule.type === 'recording') {
             return {
-                alert: rule.name,
+                record: rule.name,
                 expr: rule.query,
                 labels: rule.labels,
                 annotations: rule.annotations
             };
-        });
-    }
+        }
+    });
 
-    return { groups: [cleanGroupData] };
+    let processedGroup = {
+        name: selectedGroup.name,
+        rules: processedRules.filter(Boolean)
+    };
+
+    return { groups: [processedGroup] };
 }
 
 function convertDurationToHumanReadable(duration) {
@@ -80,86 +168,36 @@ function convertDurationToHumanReadable(duration) {
     return result;
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    codeMirrorInstance = CodeMirror.fromTextArea(document.getElementById('yamlEditor'), {
-        mode: 'yaml',
-        lineNumbers: true,
-        theme: 'monokai',
-        lineWrapping: true
-    });
+function saveRule() {
+    const editedYaml = codeMirrorInstance.getValue();
+    try {
+        const modifiedData = jsyaml.load(editedYaml);
+        const payload = JSON.stringify({ data: modifiedData });
+        const filename = encodeURIComponent(currentFilename.split('/').pop());
 
-    document.getElementById('editAlertingRulesBtn').addEventListener('click', () => {
-        fetchAndDisplayRules('alert');
-    });
-
-    document.getElementById('editRecordingRulesBtn').addEventListener('click', () => {
-        fetchAndDisplayRules('record');
-    });
-
-    document.getElementById('fileSelector').addEventListener('change', (event) => {
-        const selectedIndex = event.target.value;
-        const selectedGroup = filesData[selectedIndex];
-        currentFilename = selectedGroup.file;
-        const preprocessedData = preprocessDataForYaml(selectedGroup);
-        codeMirrorInstance.setValue(jsyaml.dump(preprocessedData));
-        document.getElementById('editorContainer').style.display = 'block';
-    });
-
-    document.getElementById('cancelBtn').addEventListener('click', () => {
-        codeMirrorInstance.setValue('');
-        document.getElementById('editorContainer').style.display = 'none';
-    });
-
-    document.getElementById('saveBtn').addEventListener('click', () => {
-        const editedYaml = codeMirrorInstance.getValue();
-        try {
-            const modifiedData = jsyaml.load(editedYaml);
-            const payload = JSON.stringify({ data: modifiedData });
-            const filename = encodeURIComponent(currentFilename.split('/').pop());
-
-            fetch(`http://localhost:5000/api/v1/rules/${filename}?recreate=true`, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: payload
-            })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`Network response was not ok: ${response.statusText}`);
-                }
-                return response.json();
-            })
-            .then(data => {
-                alert('Rule saved successfully.');
-            })
-            .catch(error => {
-                alert('Error saving rule.');
-                console.error('Error:', error);
-            });
-        } catch (error) {
-            alert('Error processing YAML.');
-            console.error('Error parsing YAML:', error);
-        }
-    });
-});
-
-function fetchAndDisplayRules(ruleType) {
-    fetch(`http://localhost:5000/api/v1/rules?type=${ruleType}`)
-        .then(response => response.json())
-        .then(json => {
-            filesData = json.data.groups;
-            const fileSelector = document.getElementById('fileSelector');
-            fileSelector.innerHTML = '';
-            json.data.groups.forEach((group, index) => {
-                const option = new Option(group.file, index);
-                option.value = index;
-                fileSelector.add(option);
-            });
-            fileSelector.style.display = 'block';
-            fileSelector.selectedIndex = -1;
+        fetch(`http://localhost:5000/api/v1/rules/${filename}?recreate=true`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: payload
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            alert('Rule saved successfully.');
+            document.getElementById('editorContainer').style.display = 'none';
         })
         .catch(error => {
-            console.error(`Error fetching ${ruleType} rules:`, error);
+            alert('Error saving rule: ' + error.message);
+            console.error('Error:', error);
         });
+    } catch (error) {
+        alert('Error processing YAML: ' + error.message);
+        console.error('Error parsing YAML:', error);
+    }
 }
