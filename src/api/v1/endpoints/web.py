23c514f2cf6f391fb8042d4e713fbaa620f3cd98
hayk96 <hayko5999@gmail.com>
2024-05-02 01:10:50 +0400

    chore(api): add description to the local routes
    Signed-off-by: hayk96 <hayko5999@gmail.com>

--- src/api/v1/endpoints/web.py
+++ src/api/v1/endpoints/web.py
@@ -1,6 +1,6 @@
 from starlette.responses import FileResponse
-from src.utils.arguments import arg_parser
 from fastapi.responses import HTMLResponse
+from src.utils.arguments import arg_parser
 from src.utils.log import logger
 from fastapi import APIRouter
 from fastapi import Request
@@ -11,15 +11,22 @@ if arg_parser().get("web.enable_ui") == "true":
     rules_management = "ui/rules-management"
     logger.info("Starting web management UI")
 
-    @router.get("/", response_class=HTMLResponse, include_in_schema=False)
+    @router.get("/", response_class=HTMLResponse,
+                description="Renders home page of this application",
+                include_in_schema=False)
     async def homepage():
         return FileResponse("ui/homepage/index.html")
 
-    @router.get("/rules-management", include_in_schema=False)
+    @router.get("/rules-management",
+                description="Renders rules management HTML page of this application",
+                include_in_schema=False)
     async def rules_management_page():
         return FileResponse(f"{rules_management}/index.html")
 
-    @router.get("/rules-management/{path}", include_in_schema=False)
+    @router.get(
+        "/rules-management/{path}",
+        description="Returns JavaScript and CSS files of the rules management page",
+        include_in_schema=False)
     async def rules_management_files(path, request: Request):
         if path in ["script.js", "style.css"]:
             return FileResponse(f"{rules_management}/{path}")
