53051b13832a00330d566f79f1d29d9ea161109d
hayk96 <hayko5999@gmail.com>
2024-05-02 01:04:03 +0400

    chore(api): update web page local endpoints
    Signed-off-by: hayk96 <hayko5999@gmail.com>

--- src/api/v1/endpoints/web.py
+++ src/api/v1/endpoints/web.py
@@ -1,27 +1,33 @@
-from fastapi.responses import HTMLResponse, Response
-from fastapi import APIRouter
 from starlette.responses import FileResponse
+from src.utils.arguments import arg_parser
+from fastapi.responses import HTMLResponse
+from src.utils.log import logger
+from fastapi import APIRouter
+from fastapi import Request
 
 router = APIRouter()
 
-homepage = "ui/homepage"
-rules_management = "ui/rules-management"
+if arg_parser().get("web.enable_ui") == "true":
+    rules_management = "ui/rules-management"
+    logger.info("Starting web management UI")
 
-@router.get("/", response_class=HTMLResponse, include_in_schema=False)
-async def home_page():
-    with open(f"{homepage}/index.html", "r") as html:
-        return html.read()
+    @router.get("/", response_class=HTMLResponse, include_in_schema=False)
+    async def homepage():
+        return FileResponse("ui/homepage/index.html")
 
-@router.get("/script.js", response_class=Response, include_in_schema=False)
-async def rules_management_page():
-    with open(f"{rules_management}/script.js", "r") as html:
-        return html.read()
+    @router.get("/rules-management", include_in_schema=False)
+    async def rules_management_page():
+        return FileResponse(f"{rules_management}/index.html")
 
-@router.get("/style.css", response_class=Response, include_in_schema=False)
-async def rules_management_page():
-    with open(f"{rules_management}/style.css", "r") as html:
-        return html.read()
-
-@router.get("/rules-management", include_in_schema=False)
-async def rules_management_page():
-    return FileResponse(f"{rules_management}/index.html")
+    @router.get("/rules-management/{path}", include_in_schema=False)
+    async def rules_management_files(path, request: Request):
+        if path in ["script.js", "style.css"]:
+            return FileResponse(f"{rules_management}/{path}")
+        sts, msg = "404", "Not Found"
+        logger.info(
+            msg=msg,
+            extra={
+                "status": sts,
+                "method": request.method,
+                "request_path": request.url.path})
+        return f"{sts} {msg}"
