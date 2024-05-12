38704fb2b678e70d0caa5b1a4a0a6008867d80c2
hayk96 <hayko5999@gmail.com>
2024-05-13 02:16:14 +0400

    feat: add background task support / fix metrics-lifecycle-policies API
    Signed-off-by: hayk96 <hayko5999@gmail.com>

---
+++ src/api/v1/endpoints/policies.py
@@ -0,0 +1,356 @@
+from src.models.policy import MetricsLifecyclePolicyCreate, MetricsLifecyclePolicyUpdate
+from fastapi import APIRouter, Response, Request, Body, status
+from src.core import policies as mlp
+from src.utils.log import logger
+from typing import Annotated
+
+router = APIRouter()
+policies = mlp.load_policies()
+
+
+@router.get("/metrics-lifecycle-policies/{name}",
+            name="Get metrics lifecycle policy by name",
+            description="Returns metrics lifecycle policy that match the provided name parameter",
+            status_code=status.HTTP_200_OK,
+            tags=["metrics-lifecycle-policies"],
+            responses={
+                200: {
+                    "description": "OK",
+                    "content": {
+                        "application/json": {
+                            "example": [
+                                {
+                                    "match": "{__name__=~'go_.+'}",
+                                    "keep_for": "7d",
+                                    "description": "This metrics lifecycle policy keeps GoLang metrics for 7 days"
+                                }
+                            ]
+                        }
+                    }
+                },
+                404: {
+                    "description": "Not Found",
+                    "content": {
+                        "application/json": {
+                            "example": [
+                                {
+                                    "status": "error",
+                                    "message": "Policy not found"}
+                            ]
+                        }
+                    }
+                }
+            }
+            )
+async def get_policy(
+        name: str,
+        request: Request,
+        response: Response,
+):
+    resp = {
+        200: {
+            "status": "success",
+            "message": "Successfully returned the requested metric lifecycle policy"},
+        404: {
+            "status": "error",
+            "message": "Policy not found"}}
+    response.status_code = 200 if policies.get(name) else 404
+    logger.info(
+        msg=resp[response.status_code]["message"],
+        extra={
+            "status": response.status_code,
+            "policy_name": name,
+            "method": request.method,
+            "request_path": request.url.path})
+    return policies.get(name) or resp[response.status_code]
+
+
+@router.get("/metrics-lifecycle-policies",
+            name="Get all metrics lifecycle policies",
+            description="Returns all metrics lifecycle policies",
+            status_code=status.HTTP_200_OK,
+            tags=["metrics-lifecycle-policies"],
+            responses={
+                200: {
+                    "description": "OK",
+                    "content": {
+                        "application/json": {
+                            "example": [
+                                {
+                                    "GoLang Policy": {
+                                        "match": "{__name__=~'go_.+'}",
+                                        "keep_for": "7d",
+                                        "message": "This policy keeps GoLang metrics for 7 days"
+                                    },
+                                    "Kubernetes Policy": {
+                                        "match": "{job='kubernetes-pods'}",
+                                        "keep_for": "10d",
+                                        "message": "This policy keeps series of job 'kubernetes-pods' for 10 days"
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                }
+            }
+            )
+async def get_policies(
+        request: Request,
+        response: Response,
+):
+    response.status_code = 200
+    logger.info(
+        msg="Successfully returned all metrics lifecycle policies",
+        extra={
+            "status": response.status_code,
+            "method": request.method,
+            "request_path": request.url.path})
+    return policies
+
+
+@router.post("/metrics-lifecycle-policies",
+             name="Create metric lifecycle policy",
+             description="Creates a new metric lifecycle policy",
+             status_code=status.HTTP_201_CREATED,
+             tags=["metrics-lifecycle-policies"],
+             responses={
+                  201: {
+                      "description": "Created",
+                      "content": {
+                          "application/json": {
+                              "example": [
+                                  {
+                                      "status": "success",
+                                      "policy_name": "GoLang Metrics Policy",
+                                      "message": "Policy created successfully"
+                                  }
+                              ]
+                          }
+                      }
+                  },
+                 400: {
+                      "description": "Bad Request",
+                      "content": {
+                          "application/json": {
+                              "example": [
+                                  {
+                                      "status": "error",
+                                      "message": "Additional properties are not allowed (time was unexpected)"
+                                  }
+                              ]
+                          }
+                      }
+                  },
+                 409: {
+                      "description": "Conflict",
+                      "content": {
+                          "application/json": {
+                              "example": [
+                                  {
+                                      "status": "error",
+                                      "message": "The requested policy already exists"
+                                  }
+                              ]
+                          }
+                      }
+                  },
+                 500: {
+                      "description": "Internal Server Error",
+                      "content": {
+                          "application/json": {
+                              "example": [
+                                  {
+                                      "status": "error",
+                                      "message": "Failed to create metric lifecycle policy"
+                                  }
+                              ]
+                          }
+                      }
+                  }
+             }
+             )
+async def create(
+        request: Request,
+        response: Response,
+        policy: Annotated[
+            MetricsLifecyclePolicyCreate,
+            Body(
+                openapi_examples=MetricsLifecyclePolicyCreate._request_body_examples,
+            )
+        ]
+):
+    data = policy.dict()
+    validation_status, response.status_code, sts, msg = mlp.validate_prom_admin_api()
+    if validation_status:
+        validation_status, response.status_code, sts, msg = mlp.validate_policy(
+            "policies_create.json", data)
+        if validation_status:
+            validation_status, response.status_code, sts, msg, val = mlp.validate_duration(
+                data.get("keep_for"))
+            if validation_status:
+                response.status_code, sts, msg = mlp.create_policy(
+                    **policy.dict(), data=policies)
+    logger.info(
+        msg=msg,
+        extra={
+            "status": response.status_code,
+            "policy_name": policy.dict().get("name"),
+            "method": request.method,
+            "request_path": request.url.path})
+    return {
+        "status": sts,
+        "policy_name": policy.dict().get("name"),
+        "message": msg
+    }
+
+
+@router.patch("/metrics-lifecycle-policies/{name}",
+              name="Update metrics lifecycle policy by name",
+              description="Updates specific metrics lifecycle policy",
+              status_code=status.HTTP_200_OK,
+              tags=["metrics-lifecycle-policies"],
+              responses={
+                  200: {
+                      "description": "OK",
+                      "content": {
+                          "application/json": {
+                              "example": [
+                                  {
+                                      "status": "success",
+                                      "message": "Policy updated successfully"
+                                  }
+                              ]
+                          }
+                      }
+                  },
+                  400: {
+                      "description": "Bad Request",
+                      "content": {
+                          "application/json": {
+                              "example": [
+                                  {
+                                      "status": "error",
+                                      "message": "Additional properties are not allowed ('name' was unexpected)"
+                                  }
+                              ]
+                          }
+                      }
+                  },
+                  404: {
+                      "description": "Not Found",
+                      "content": {
+                          "application/json": {
+                              "example": [
+                                  {
+                                      "status": "error",
+                                      "message": "Policy not found"
+                                  }
+                              ]
+                          }
+                      }
+                  },
+                  500: {
+                      "description": "Internal Server Error",
+                      "content": {
+                          "application/json": {
+                              "example": [
+                                  {
+                                      "status": "error",
+                                      "message": "Failed to create metric lifecycle policy"
+                                  }
+                              ]
+                          }
+                      }
+                  }
+              }
+              )
+async def update(
+        name: str,
+        request: Request,
+        response: Response,
+        policy: Annotated[
+            MetricsLifecyclePolicyUpdate,
+            Body(
+                openapi_examples=MetricsLifecyclePolicyUpdate._request_body_examples,
+            )
+        ]
+):
+    data = policy.dict(exclude_unset=True)
+    validation_status, response.status_code, sts, msg = mlp.validate_prom_admin_api()
+    if validation_status:
+        validation_status, response.status_code, sts, msg = mlp.validate_policy(
+            "policies_update.json", data)
+        if validation_status:
+            validation_status, response.status_code, sts, msg, val = mlp.validate_duration(
+                data.get("keep_for"))
+            if validation_status:
+                response.status_code, sts, msg = mlp.update_policy(
+                    name=name, data=data, policies=policies)
+    logger.info(
+        msg=msg,
+        extra={
+            "status": response.status_code,
+            "policy_name": name,
+            "method": request.method,
+            "request_path": request.url.path})
+    return {
+        "status": sts,
+        "policy_name": name,
+        "message": msg
+    }
+
+
+@router.delete("/metrics-lifecycle-policies/{name}",
+               name="Delete metrics lifecycle policy",
+               description="Deletes specific metrics lifecycle policy",
+               status_code=status.HTTP_204_NO_CONTENT,
+               tags=["metrics-lifecycle-policies"],
+               responses={
+                   204: {
+                    "description": "No Content"
+                   },
+                   404: {
+                       "description": "Not Found",
+                       "content": {
+                           "application/json": {
+                               "example": [
+                                   {
+                                       "status": "error",
+                                       "message": "Policy not found"
+                                   }
+                               ]
+                           }
+                       }
+                   },
+                   500: {
+                       "description": "Internal Server Error",
+                       "content": {
+                           "application/json": {
+                               "example": [
+                                   {
+                                       "status": "error",
+                                       "message": "Failed to delete metrics lifecycle policy"
+                                   }
+                               ]
+                           }
+                       }
+                   }
+               }
+               )
+async def delete(
+        name: str,
+        request: Request,
+        response: Response
+):
+
+    response.status_code, sts, msg = mlp.delete_policy(
+        name=name, policies=policies)
+    logger.info(
+        msg=msg,
+        extra={
+            "status": response.status_code,
+            "method": request.method,
+            "request_path": request.url.path})
+    return {
+        "status": sts,
+        "message": msg} if response.status_code != 204 else response.status_code
