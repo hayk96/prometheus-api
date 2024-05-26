ece66d74d879710c8f30eb8788d4db594df8a174
Hayk Davtyan <46712946+hayk96@users.noreply.github.com>
2024-05-26 22:19:07 +0400

    Add new API metrics-lifecycle-policies (#19)
    * patch(api) initial version of MLP API
    
    Signed-off-by: hayk96 <hayko5999@gmail.com>
    
    * feat: add background task support / fix metrics-lifecycle-policies API
    
    Signed-off-by: hayk96 <hayko5999@gmail.com>
    
    * chore: bump app version
    
    Signed-off-by: hayk96 <hayko5999@gmail.com>
    
    * docs(examples): add new flag in container args
    
    Signed-off-by: hayk96 <hayko5999@gmail.com>
    
    * Update CHANGELOG.md #major
    
    Signed-off-by: hayk96 <hayko5999@gmail.com>
    
    ---------
    
    Signed-off-by: hayk96 <hayko5999@gmail.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ src/core/policies.py
@@ -0,0 +1,123 @@
+from jsonschema import validate, exceptions
+from src.utils.arguments import arg_parser
+from src.utils.settings import prom_info
+from src.utils.log import logger
+from pytimeparse2 import parse
+import json
+
+rule_path = arg_parser().get("rule.path")
+prom_addr = arg_parser().get("prom.addr")
+policies_data_file = ".policies.json"
+prom_storage_retention_human = prom_info(
+    prom_addr, "/runtimeinfo")["data"]["storageRetention"]
+
+
+def sync_to_file(data) -> None:
+    """This function saves metrics lifecycle policies
+    JSON object into file once it's updated"""
+    global rule_path, policies_data_file
+    with open(f"{rule_path}/{policies_data_file}", "w") as f:
+        f.write(json.dumps(data))
+    logger.debug(
+        f"Policy has been updated in file {rule_path}/{policies_data_file}.")
+
+
+def create_policy(name: str, match: str, keep_for: str,
+                  description: str, data: dict) -> tuple[int, str, str]:
+    """This function creates a new metrics lifecycle policy"""
+    global rule_path, policies_data_file
+    if name in data.keys():
+        return 409, "error", "The requested policy already exists"
+    try:
+        data[name] = {
+            "match": match,
+            "keep_for": keep_for,
+            "description": description
+        }
+        sync_to_file(data)
+    except BaseException as e:
+        return 500, "error", f"Failed to save policy. {e}"
+    return 200, "success", "Policy created successfully"
+
+
+def update_policy(name: str, data: dict,
+                  policies: dict) -> tuple[int, str, str]:
+    """This function updates the existing metrics lifecycle policy"""
+    global rule_path, policies_data_file
+
+    if name not in policies.keys():
+        return 404, "error", "Policy not found"
+    try:
+        policies[name].update(data)
+        sync_to_file(policies)
+    except BaseException as e:
+        return 500, "error", f"Failed to update policy. {e}"
+    return 200, "success", "Policy updated successfully"
+
+
+def delete_policy(name: str, policies: dict) -> tuple[int, str, str]:
+    """This function deletes a metrics lifecycle policy"""
+    global rule_path, policies_data_file
+
+    if name not in policies.keys():
+        return 404, "error", "Policy not found"
+    try:
+        del policies[name]
+        sync_to_file(policies)
+    except BaseException as e:
+        return 500, "error", f"Failed to update policy. {e}"
+    return 204, "success", "Policy deleted successfully"
+
+
+def load_policies() -> dict:
+    """This function loads metrics lifecycle policies
+     object into memory at runtime of the application"""
+    global rule_path, policies_data_file
+    policies = dict()
+    try:
+        with open(f"{rule_path}/{policies_data_file}", "r") as f:
+            policies = json.loads(f.read())
+    except FileNotFoundError:
+        logger.debug("No metrics lifecycle policies configured yet")
+    except BaseException as e:
+        logger.error(f"Failed to load metrics lifecycle policies. {e}")
+    finally:
+        return policies
+
+
+def validate_policy(schema_file, data) -> tuple[bool, int, str, str]:
+    """
+    Validates the rule object provided by
+    the user against the required schema.
+    """
+    schema_file = f"src/schemas/{schema_file}"
+    with open(schema_file) as f:
+        schema = json.load(f)
+    try:
+        validate(instance=data, schema=schema)
+    except exceptions.ValidationError as e:
+        return False, 400, "error", e.args[0]
+    return True, 200, "success", "Policy is valid"
+
+
+def validate_duration(val) -> tuple[bool, int, str, str, int]:
+    """
+    This function compares the value of the 'keep_for'
+    field with the retention time of the Prometheus server
+    """
+    prom_storage_retention_seconds = parse(prom_storage_retention_human)
+    val_seconds = parse(val)
+    if val_seconds >= prom_storage_retention_seconds:
+        return False, 422, "error", f"Invalid duration: 'keep_for' must be less than Prometheus " \
+            f"TSDB storage retention time, which is {prom_storage_retention_human}", 0
+    return True, 200, "success", "Duration is valid", val_seconds
+
+
+def validate_prom_admin_api() -> tuple[bool, int, str, str]:
+    prom_admin_api_status = prom_info(
+        prom_addr, "/flags")["data"]["web.enable-admin-api"]
+    if prom_admin_api_status == "false":
+        return False, 500, "error", "Metrics lifecycle policy API requires enabling Prometheus admin APIs. " \
+                                    "For more information on configuration, check out this documentation " \
+                                    "https://prometheus.io/docs/prometheus/latest/querying/api/#tsdb-admin-apis"
+    return True, 200, "success", "Prometheus admin API is enabled"
---
+++ src/tasks/policies.py
@@ -0,0 +1,63 @@
+from src.core.policies import load_policies
+from src.utils.arguments import arg_parser
+from src.utils.log import logger
+from pytimeparse2 import parse
+import requests
+import time
+
+prom_addr = arg_parser().get("prom.addr")
+
+
+def delete_series(policy_name: str, policy: dict) -> None:
+    """
+    This function calls two Prometheus endpoints:
+    * POST /api/v1/admin/tsdb/delete_series
+    * POST /api/v1/admin/tsdb/clean_tombstones
+    User-defined policies passed to this function
+    perform cleanup based on the specified policy settings.
+    """
+    time_range = time.time() - parse(policy["keep_for"])
+    start_time = time.time()
+    try:
+        r = requests.post(
+            f'{prom_addr}/api/v1/admin/tsdb/delete_series?match[]={policy["match"]}&end={time_range}')
+    except BaseException as e:
+        logger.error(e, extra={"policy_name": policy_name})
+    else:
+        if r.status_code != 204:
+            logger.error(f"Failed to delete series, {r.json().get('error')}", extra={
+                         "status": r.status_code, "policy_name": policy_name})
+            return
+        try:
+            r = requests.post(
+                f'{prom_addr}/api/v1/admin/tsdb/clean_tombstones')
+        except BaseException as e:
+            logger.error(e, extra={"policy_name": policy_name})
+            return
+        else:
+            if r.status_code != 204:
+                logger.error(f"Failed to clean tombstones, {r.json().get('error')}", extra={
+                             "status": r.status_code, "policy_name": policy_name})
+                return
+        exec_time = float("{:.2f}".format(time.time() - start_time))
+        logger.debug("Task cleanup time-series has been successfully completed",
+                     extra={"policy_name": policy_name, "exec_time": exec_time})
+        return
+
+
+def task_run_policies():
+    """
+    This function loops over user-defined metrics lifecycle
+    policies and executes the cleanup job one by one
+    """
+    policies = load_policies()
+    if policies:
+        logger.debug(
+            f"Found {len(policies)} metrics lifecycle {'policies' if len(policies) > 1 else 'policy'}. "
+            f"Starting job to cleanup time-series.")
+        for p in policies:
+            logger.debug(
+                "Task cleanup time-series is in progress", extra={
+                    "policy_name": p, "match": policies[p]["match"],
+                    "keep_for": policies[p]["keep_for"]})
+            delete_series(policy_name=p, policy=policies[p])
